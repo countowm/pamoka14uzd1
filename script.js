2ebd3290b2442b1eae80c42325b812630c8e8bc6
countowm <countowm@gmail.com>
2022-06-08 08:40:59 +0300

    padariau

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    let x;
+    let y;
+    let rez;
+    x = 6;
+    y = 18;
+    rez = staciakampioperimetras(x + y)
+
+    console.log(rez);
+}
+
+
+    function staciakampioperimetras(rez){
+        return rez * 2;
+    }
+    
